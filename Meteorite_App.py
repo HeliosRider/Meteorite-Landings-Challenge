552f65919d182e0bb2f292aebff08639cc327b2b
Jonathan Calloway <stscsrtrd@gmail.com>
2025-01-03 13:21:01 -0500

    Updated Meteorite App under Meteorite_App2

--- Meteorite_App.py
+++ Meteorite_App.py
@@ -1,48 +1,38 @@
-#Import dependencies
+# Import Python SQL toolkit and Object Relational Mapper
 
-import os
-import csv
-import numpy as np
-import pandas as pd
-import datetime as dt
-
-# Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, Column, Integer, String, Float
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
-MetLandings = os.path.join("C:\Class\Challenges\MOD 16 Challenge\Meteorite-Landings-Challenge\Resources\Meteorite_Data.csv")
+# Create an SQLite engine
+engine = create_engine('sqlite:///Meteorites_data.db')
 
-# Verify database incorporates the correct csv data
+ #Create the session (link) from Python to the DB
+session = Session (engine)
 
-# Open and read csv
-with open(MetLandings, encoding='UTF-8') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    
-    # Read the header row of csv file
-    csv_header = next(csv_reader)
-    
-    # Print the header
-    print("Header:", csv_header)
-    
-    # Initialize a list to store the rows
-    data_rows = []
-    
-    # Read each row in the csv
-    for row in csv_reader:
-        data_rows.append(row)
+# Define the base class
+Base = declarative_base()
 
-# Print the data rows
-    for row in data_rows:
-        print(row)
+# Define the meteorites table as a class
+class Meteorite(Base):
+    __tablename__ = 'meteorites'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    mass = Column(Integer)
+    year = Column(Integer)
+    GeoLocation = Column(String)
 
-# Create our session (link) from Python to the DB
-session = Session (MetLandings)
 
 #################################################
 # Flask Setup
@@ -55,21 +45,81 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/")
+def welcome():
+    """list all available API routes"""
+    return """
+<h1>Welcome to the Meteorite Landings Application API!</h1>
 
-@app.route("/api/v1.0/name")
+<img src="https://img.freepik.com/free-photo/milky-way-illum…d-generated-by-ai_24640-93529.jpg?semt=ais_hybrid?" alt="Meteorite Landings"/>
+<p>Name:</p>
+<ul>
+  <li><a href="/api/v1.0/Name">/api/v1.0/Name</a></li>
+</ul>
+<p>Mass(g):</p>
+<ul>
+  <li><a href="/api/v1.0/Mass(g)">/api/v1.0/Mass(g)</a></li>
+</ul>
+<p>Year:</p>
+<ul>
+  <li><a href="/api/v1.0/Year">/api/v1.0/Year</a></li>
+</ul>
+<p>GeoLocation:</p>
+<ul>
+  <li><a href="/api/v1.0/GeoLocation">/api/v1.0/GeoLocation</a></li>
+</ul>
+</html>
+"""
+
+@app.route("/api/v1.0/Name")
 def names():
-
+   
     """Return a list of all meteorite names"""
     # Query all meteorites
-    results = session.query(MetLandings.name).all()
+    results = session.query(Meteorite.name).all()
+    names = [name for name, in results]
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    return jsonify(names)
 
-    return jsonify(all_names)
+# #app routing for meteorites mass 
+@app.route("/api/v1.0/Mass(g)")
+def mass():
+  
+    """Return a list of the mass of all meteorites"""
+    # Query all meteorites
+    results = session.query(Meteorite.mass).all()
+    names = [name for name, in results]
+
+    session.close()
+
+    return jsonify(names)
+
+# #app routing for year meteorites fell
+@app.route("/api/v1.0/Year")
+def year():
+
+    """Return a list of the impact year of all meteorites"""
+    # Query all meteorites
+    results = session.query(Meteorite.year).all()
+    names = [name for name, in results]
+
+    session.close()
+
+    return jsonify(names)
+
+# #app routing lat/Lon
+@app.route("/api/v1.0/GeoLocation")
+def GeoLocation():
+
+    """Return a list of the lat/lon of all meteorites"""
+    # Query all meteorites
+    results = session.query( Meteorite.GeoLocation).all()
+    names = [name for name, in results]
+
+    session.close()
+
+    return jsonify(names)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
